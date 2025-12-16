c4a0bc137459fcdf64f82c95ddb82049ff89aa1b
mqnoy <qnoy.social@gmail.com>
2025-12-16 22:48:44 +0700

    chore: added steam for gaming

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -182,6 +182,13 @@
     };
   };
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
